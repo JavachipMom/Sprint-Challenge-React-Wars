28895857ac6fffca375f3eaba95a03f924e68cc6
JavachipMom <Nicolesp4ever@gmail.com>
2019-11-24 23:03:04 -0600

    Made initial changes to components and added dependencies..

---
+++ starwars/src/components/StarWarsCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const StarWarsCard = ({ character }) => {
+    return (
+        <div className="character-list">
+            <h2></h2>
+        </div>
+    );
+};
+
+export default StarWarsCard;
